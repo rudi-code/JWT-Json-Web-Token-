218cbadb6f742f46713488d4680f83d2fdc2330a
rudip <rudi.purnomo@quadras.co.id>
2020-01-15 19:30:39 +0700

    add jwt config dll add jwt

--- jwt/src/main/java/id/co/ogya/jwt/controller/HomeController.java
+++ jwt/src/main/java/id/co/ogya/jwt/controller/HomeController.java
@@ -1,5 +1,14 @@
 package id.co.ogya.jwt.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+	
+	@GetMapping("/")
+	String index() {
+		return "redirect:/swagger-ui.html";
+	}
 
 }
